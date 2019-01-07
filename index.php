590e432f7aee9d1263462c5b96c3245c0d9381b6
wouerner <wouerner@protonmail.com>
2019-01-07 19:51:46 -0200

    removendo

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Wouerner\MercadoBitcoin as MercadoBitcoin;
+use Wouerner\MercadoBitcoin\Api as MercadoBitcoin;
 
 $dotenv = new Dotenv\Dotenv(__DIR__);
 $dotenv->load();
@@ -12,5 +12,25 @@ $secredo = getenv('SECREDO');
 $mcb = new MercadoBitcoin($id, $secredo);
 
 $info = $mcb->getAccountInfo();
-echo "<pre>";
-var_dump($info);
+
+$balances = $info['response_data']['balance'];
+
+foreach($balances as $key => $balance){
+    $aux[$key]['total'] = (float)$balance['total'];
+    if($key != 'brl'){
+        $aux[$key]['ticker'] = (float)$mcb->ticker(strtoupper($key))['ticker']['last'];
+    }
+}
+
+foreach($aux as $key => $balance){
+    if($key != 'brl'){
+        $aux[$key]['valor'] = (float)($balance['total'] * $balance['ticker']);
+    }
+}
+
+$valorTotal = array_sum(array_column($aux, 'valor')) + $aux['brl']['total'];
+
+$aux['valorTotal'] = ($valorTotal);
+
+header('Content-type: application/json');
+echo json_encode($aux);
